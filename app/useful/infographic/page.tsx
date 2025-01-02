b04df16640797c4b7c82d73a783f540535c940f8
stripyq <stripyMM@protonmail.com>
2025-01-02 22:27:45 +0100

    tada

--- app/useful/infographic/page.tsx
+++ app/useful/infographic/page.tsx
@@ -4,8 +4,8 @@ export default function InfographicPage() {
       <h1 className="text-3xl font-bold mb-6">QL players snapshot 2024</h1>
       <div className="w-full max-w-[2500px] relative">
         <img
-          src="../infographic.png"
-          alt="HoQ infographic"
+          src="/quakesettings/useful/infographic.png"
+          alt="HoQ Infographic"
           className="w-full h-auto"
           loading="lazy"
         />
