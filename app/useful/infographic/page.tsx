8f8edf4c6be26eb2314a84b880e777b9d9d1c068
stripyq <stripyMM@protonmail.com>
2025-01-02 21:41:12 +0100

    infografic

--- app/useful/infographic/page.tsx
+++ app/useful/infographic/page.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image"
-
 export default function InfographicPage() {
   return (
     <div className="flex flex-col items-center p-4">
       <h1 className="text-3xl font-bold mb-6">Quake Live Infographic Guide</h1>
       <div className="w-full max-w-[2500px] relative">
-        <Image
-          src="/quake-live-infographic.png"
-          width={2500}
-          height={29103}
+        <img
+          src="/quakesettings/infographic.png"
           alt="Quake Live Infographic"
           className="w-full h-auto"
-          priority
         />
       </div>
     </div>
